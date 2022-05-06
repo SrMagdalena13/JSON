e6780884b6dbc397605f4b3a275e40050711b6b6
Sergio Fernandez Martinez <sfernandezmartinez@danielcastelao.org>
2022-05-06 09:44:57 +0200

    Prueba con código

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,20 @@
 import org.json.JSONObject;
 public class Test {
     public static void main(String args[]){
-        JSONObject jo = new JSONObject("{ 'abc' : 'def' }");
+        String cadenaJSON= " { "+
+                " \"data\": {"+
+                "\"id\": 2,"+
+                "       \"email\": \"janet.weaver@reqres.in\","+
+                "\"first_name\": [\"Janet\",\"Brown\"],"+
+                "\"vivo\": \"false\","+
+                "\"avatar\": \"https://reqres.in/img/faces/2-image.jpg\""+
+                "},"+
+                "\"support\": {"+
+                "\"url\": \"https://reqres.in/#support-heading\","+
+                "\"text\": \"To keep ReqRes free, contributions towards server costs are appreciated!\""+
+                "}"+
+                "}";
+        JSONObject jo = new JSONObject(/*"{ 'abc' : 'def' }"*/ cadenaJSON);
         System.out.println(jo.toString());
     }
 }
